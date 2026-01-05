79bcfa6f524a12e0a496f2987dd885a5f65748b0
Surya Prakash <surya.jinagam@gmail.com>
2026-01-05 16:36:48 +0530

    establish database connection before starting the server

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,7 +1,29 @@
 import app from "./app";
+import dotenv from "dotenv";
+import prisma from "../lib/prisma";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+const connnectDB = async () => {
+	try {
+		await prisma.$connect();
+		console.log("Database connected successfully");
+		
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+
+	} catch (error) {
+		console.error("Database connection failed:", error);
+		process.exit(1);
+	}
+};
+
+connnectDB();
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
